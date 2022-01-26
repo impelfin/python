b33e7f9d3d5da551f26fb07e6e75d72a8db62a08
JunHyen Mun <impelfin@gmail.com>
2022-01-27 01:28:17 +0900

    .,

--- plaidml-venv/bin/activate.fish
+++ plaidml-venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lune/Documents/GitHub/python/plaidml-venv"
+set -gx VIRTUAL_ENV "/Users/Lune/Documents/GitHub/python/plaidml-venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
